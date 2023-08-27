f40e2196c8cf43c5fa590b436943819fea5e5c5d
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-08-27 15:03:59 -0400

    Ads more comments

--- main.py
+++ main.py
@@ -12,49 +12,54 @@
 # Client: 402 SWEG
 # ==============================================================================
 
+# ===================================
+# 1. Import necessary libraries
+# ===================================
 import cv2
 import numpy as np
 from scipy.optimize import curve_fit
 import csv
 
-# User-configurable constants
-# RGB lower and upper bounds to identify the "screen" region
+# ===================================
+# 2. Define constants
+# ===================================
+
+# User-configurable constants for color filtering
 LOWER_GREEN = np.array([33, 45, 45])
 UPPER_GREEN = np.array([92, 260, 260])
-
-# RGB lower and upper bounds to identify the "wave" region
 LOWER_WAVE_COLOR = np.array([78, 145, 115])
 UPPER_WAVE_COLOR = np.array([102, 260, 260])
 
-# Kernel size and dilation/erosion iterations for image filtering
+# Image filtering parameters
 KERNEL_SIZE = np.ones((5, 5), np.uint8)
 DILATE_ITERATIONS = 1
 ERODE_ITERATIONS = 1
 
-# File path to the video to analyze
+# Video configuration
 VIDEO_PATH = 'CW Signal.mp4'
-
-# Key to quit the video playback
 QUIT_KEY = 'q'
 
-# Define a parabolic function for curve fitting
+# ===================================
+# 3. Define utility functions
+# ===================================
+
 def parabola(x, a, b, c):
     """Defines a parabolic function."""
     return a * x ** 2 + b * x + c
 
-# Apply a color filter based on RGB lower and upper bounds
 def apply_color_filter(frame, lower_bound, upper_bound):
+    """Apply a color filter based on RGB lower and upper bounds."""
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_bound, upper_bound)
     return mask
 
-# Find and return the largest contour in a given binary mask
 def find_largest_contour(mask):
+    """Find and return the largest contour in a given binary mask."""
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return max(contours, key=cv2.contourArea) if contours else None
 
-# Identify the screen area within a video frame based on its color
 def find_screen(frame):
+    """Identify the screen area within a video frame based on its color."""
     mask = apply_color_filter(frame, LOWER_GREEN, UPPER_GREEN)
     largest_contour = find_largest_contour(mask)
     
@@ -63,8 +68,8 @@ def find_screen(frame):
     else:
         return None
 
-# Find and process the wave within a video frame
 def find_wave(frame):
+    """Find and process the wave within a video frame."""
     mask = apply_color_filter(frame, LOWER_WAVE_COLOR, UPPER_WAVE_COLOR)
     largest_contour = find_largest_contour(mask)
 
@@ -78,9 +83,10 @@ def find_wave(frame):
 
     return mask, np.where(mask)
 
-# Analyze and extract wave characteristics
+
 def process_wave(wave_x, wave_y):
-    if len(wave_x) > 0 and len(wave_y) > 0:
+    """Analyze and extract wave characteristics."""
+    if wave_x.size > 0 and wave_y.size > 0:
         params, _ = curve_fit(parabola, wave_x, wave_y)
         a, b, c = params
 
@@ -91,11 +97,24 @@ def process_wave(wave_x, wave_y):
         return center_freq, min_amplitude, max_amplitude, center_amplitude
     return None
 
-# Main execution function
+# ===================================
+# 4. Main execution functions
+# ===================================
+
+def print_wave_characteristics(result, frame_number, fps):
+    """Print extracted wave characteristics."""
+    timestamp = frame_number / fps
+    center_freq, min_amplitude, max_amplitude, center_amplitude = result
+    print(f"Timestamp: {timestamp} seconds")
+    print(f"Center Frequency: {center_freq}")
+    print(f"Minimum Amplitude: {min_amplitude}")
+    print(f"Maximum Amplitude: {max_amplitude}")
+    print(f"Center Amplitude: {center_amplitude}\n")
+
 def main():
-    # Initialization and video metadata fetching
-    video_path = VIDEO_PATH
-    cap = cv2.VideoCapture(video_path)
+    """Main execution function."""
+    # Video setup
+    cap = cv2.VideoCapture(VIDEO_PATH)
     if not cap.isOpened():
         print("Error: Could not open the video file.")
         return
@@ -104,17 +123,14 @@ def main():
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     print(f"Playing video with dimensions: {frame_width}x{frame_height} and {fps} FPS.")
 
-    # Initialize some variables
     detected_signals = []
 
     # Main video processing loop
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
-            print("Reached the end of the video or an error occurred.")
             break
 
-        # Detect screen and wave regions
         screen_info = find_screen(frame)
         if screen_info:
             x, y, w, h = screen_info
@@ -122,13 +138,9 @@ def main():
             mask, (wave_x, wave_y) = find_wave(cropped_frame)
             result = process_wave(wave_x, wave_y)
 
-            # Additional stabilization logic can be added here
-
-            # If a wave was successfully processed
             if result:
                 print_wave_characteristics(result, cap.get(cv2.CAP_PROP_POS_FRAMES), fps)
-                detected_signals.append(result)  # Store detected signal information
-
+                detected_signals.append(result)
             cv2.imshow('Video', mask)
         else:
             cv2.imshow('Video', frame)
@@ -136,7 +148,7 @@ def main():
         if cv2.waitKey(1000 // fps) & 0xFF == ord(QUIT_KEY):
             break
 
-    # Save the detected signal information to a CSV file
+    # Save results to CSV
     csv_file = 'detected_signals.csv'
     with open(csv_file, mode='w', newline='') as file:
         writer = csv.writer(file)
@@ -151,16 +163,8 @@ def main():
     cv2.destroyAllWindows()
     print("Video playback is done.")
 
-# Function to print extracted wave characteristics
-def print_wave_characteristics(result, frame_number, fps):
-    timestamp = frame_number / fps
-    center_freq, min_amplitude, max_amplitude, center_amplitude = result
-    print(f"Timestamp: {timestamp} seconds")
-    print(f"Center Frequency: {center_freq}")
-    print(f"Minimum Amplitude: {min_amplitude}")
-    print(f"Maximum Amplitude: {max_amplitude}")
-    print(f"Center Amplitude: {center_amplitude}\n")
-
-# Entry point of the script
+# ===================================
+# 5. Script entry point
+# ===================================
 if __name__ == "__main__":
     main()
